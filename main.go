218a5eff1bbd49c66b337f9f5ccae720980ace45
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2026-05-12 20:59:04 +0200

    Zrobiono cwiczenie

--- main.go
+++ main.go
@@ -3,7 +3,13 @@ package main
 func SumExercise(nums []int64) int64 {
 	// Write code for the exerxise here
 	// Tip: return -1 for avoiding compiler errors, you can safely remove it
-	return -1
+	sum := int64(0)
+
+	for _, num := range nums {
+		sum += num
+	}
+
+	return sum
 }
 
 func main() {
